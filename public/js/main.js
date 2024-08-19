3aaa84e629af64b53c60ca64a70162bb96aaea74
emanuelsobral <emanuelssobral@gmail.com>
2024-08-19 19:12:45 -0300

    removed 100% GEMINI and FIREBASE, IA WITH ALGORITM INSTED

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Load environment variables
-import dotenv from 'dotenv';
-dotenv.config();
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
 const fetchData = async () => {
     try {
         const response = await fetch('http://localhost:3000/data', {
